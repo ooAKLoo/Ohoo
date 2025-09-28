d0a84d5aab1763e701587597a835ba867a959e62
yangdongju <2797208347@qq.ocm>
2025-09-28 18:59:43 +0800

    现在可以正常构建并获得麦克风权限了。

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    // macOS Info.plist权限设置
+    #[cfg(target_os = "macos")]
+    {
+        println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=10.13");
+    }
+    
     tauri_build::build()
 }
